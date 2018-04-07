7fdf3dbd29de225454fa69abb19c7555be6bfa90
chenxianjun <1004178387@qq.com>
2018-04-07 15:08:18 +0800

    优化
    优化

--- src/main/java/com/ccloomi/cdte/CDTemplate.java
+++ src/main/java/com/ccloomi/cdte/CDTemplate.java
@@ -22,8 +22,6 @@ public class CDTemplate {
 	}
 
 	public void render(FelContext ctx, OutputStream out) {
-		//TODO
-		System.out.println(voutList);
 		try {
 			for (Vout v : voutList) {
 				v.render(ctx, out);
